b2307a323cf86e89e95afa9881041e20ed686c29
oloojacob <82253460+oloojacob@users.noreply.github.com>
2025-03-01 13:02:37 +0300

    Added second file and linked it to the home page_Oloo

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
-@app.route("/")
-def hello():
-    return "Hello, World!"
-if __name__ == "__main__":
+
+@app.route('/')
+def home():
+    return '''
+    <h1>Hello, World!</h1>
+    <p><a href="/second">Click here to view the second file</a></p>
+    '''
+
+@app.route('/second')
+def second():
+    return '''
+    <h1>Welcome to the Second Page</h1>
+    <p>This is the second file linked to the first page.</p>
+    <p><a href="/">Go back to Home</a></p>
+    '''
+
+if __name__ == '__main__':
     app.run(debug=True)
